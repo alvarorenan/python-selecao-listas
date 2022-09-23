fd975f926b83a77ba4d52a9cfafd349d595ef8f8
alvaro.carvalho.703 <alvaro.carvalho.703@ufrn.edu.br>
2022-09-23 18:59:04 -0300

    feat: q4

--- questao4.py
+++ questao4.py
@@ -1,2 +1,26 @@
 n = int(input())
-if (n)
+primo = True;
+sophie = True;
+if n==1:
+    sophie = False
+    primo = False
+for i in range(2, n):
+    if n%i == 0:
+       primo = False;
+       break
+if primo:
+    for i in range(2, n*2+1):
+        if (n*2+1)%i == 0:
+            sophie = False;
+            break
+if primo and sophie:
+    print('Numero primo de Sophie-Germain')
+elif primo:
+    print('Numero primo')
+else:
+    print('Numero nao primo')
+
+
+
+
+
